354434246366548c8186499d732fb2ebb10cbff8
knil-maloon <linkedfrends@gmail.com>
2021-07-27 08:51:01 -0500

    response rates first draft

--- dashboard.php
+++ dashboard.php
@@ -15,9 +15,10 @@ if(($_SESSION[$_GET['pid'] . "_startDate"] == "" || $_SESSION[$_GET['pid'] . "_s
 
 $array_questions = array(
         1 => "Participant perception",
-        2 => "Reasons for joining a study",
-        3 => "Reasons for leaving a study",
-        4 => "Reasons for staying in a study"
+        2 => "Response/Completion Rates",
+        3 => "Reasons for joining a study",
+        4 => "Reasons for leaving a study",
+        5 => "Reasons for staying in a study"
 );
 
 $array_study = array(
@@ -122,6 +123,7 @@ if(!empty($_GET['dash']) && ProjectData::startTest($_GET['dash'], '', '', $_SESS
     $row_questions_1 = array(0 => "rpps_s_q1",1 => "rpps_s_q17", 2 => "rpps_s_q18", 3 => "rpps_s_q19", 4 => "rpps_s_q20", 5 => "rpps_s_q21",
                         6 => "rpps_up_q66", 7 => "rpps_s_q22", 8 => "rpps_s_q23", 9 => "rpps_s_q24", 10 => "rpps_s_q25", 11 => "rpps_up_q65",
                         12 => "rpps_up_q67", 13 => "rpps_s_q57");
+    $row_questions_2 = array(1 => "any", 2 => "partial", 3 => "complete", 4 => "breakoffs");
     $study_options = $module->getChoiceLabels("rpps_s_q" . $study, $project_id);
     $graph = array();
 
@@ -151,7 +153,10 @@ if(!empty($_GET['dash']) && ProjectData::startTest($_GET['dash'], '', '', $_SESS
         $top_box_popover_content = "<div style='padding-bottom: 20px'><strong>Definitions:</strong> “Top box” scores are created by calculating the percentage of survey respondents who chose the most positive score for a given item response scale.  A variation, the  'Top 2 box' score is the total of the top two categories in a table. For example, the overall Rating question is scores using a Top 2 Box score; participants reply using a numeric scale from “0 (Worst)” to “10 (Best)”.  The Top 2 Box scores based on the combination of the responses “9” and “10”.</div>
                                 <div><strong>Top-Box versus mean scores:</strong> Top Box scores are easy to understand because they clearly identify the respondents who fall into a given category, usually the optimal category from a marketing or customer experience perspective. However, taken alone, Top Box scores leave out important information about how many respondents were close to, or far from the top category. In contrast, mean scores provide a summary measure that includes all the data, including lower and lowest scores, but it may be harder to interpret the significance of a 0.1  or 1 or 5 point difference between two mean scores, especially if the sample size is large. The frequency distribution of all of the response data provides the richest data about the range and number of responses, but lacks the simplicity of summary scores. It can be most valuable to use summary scores like Top Box or Means to follow overall trends, and frequency distributions to understand the details when designing interventions for change. When assessing change in a score, it is important to compare measures of the same type.</div>";
         $top_box_popover_info = ' <a tabindex="0" role="button" data-container="body" data-toggle="popover" data-trigger="hover" data-html="true" data-placement="right" data-content="'.$top_box_popover_content.'"><i class="fas fa-info-circle fa-fw infoIcon" aria-hidden="true"></i></a>';
+    }else if($question == 2){
+        $score_title = "% Responding";
     }
+
     $table = '<div class="optionSelect" id="loadTable">
                <div style="/* padding-bottom: 10px; */width: 621px;" class="forme">
                 <img src="'.$module->getUrl('epv-2colorhorizontal1300__1_.jpg').'" width="300px" style="/* padding-top: 20px */">
@@ -220,34 +225,34 @@ if(!empty($_GET['dash']) && ProjectData::startTest($_GET['dash'], '', '', $_SESS
     }
     $table .= '</tr>';
 
-        $table .= '<tr>';
-        $table .= '<td class="question"></td>';
-        $table .= '<td></td>';
-        foreach ($study_options as $indexstudy => $col_title) {
-            if($study == 62) {
-                $table .="<style>.dal_task>div>span {
-                            display: block;
-                            margin-left: 38px;
-                            color: #5592c6;
-                        }</style>";
-                if ($indexstudy == 6) {
-                    $table .= '<td><i class="fas fa-plus-circle fa-fw" id="etnicityPlus" aria-hidden="true" onclick="etnicity_change_icon(this.id)" symbol="0"></i></td>';
-                } else if ($indexstudy != 1) {
-                    $table .= '<td class="hide" etnicity="1"></td>';
-                } else if ($indexstudy == 1) {
-                    $table .= '<td></td>';
-                }
-            }else{
-                $table .="<style>.dal_task>div>span {
-                            display: block;
-                            margin-left: 20px;
-                            color: #5592c6;
-                        }</style>";
-                $table .= '<td>&nbsp;</td>';
+    $table .= '<tr>';
+    $table .= '<td class="question"></td>';
+    $table .= '<td></td>';
+    foreach ($study_options as $indexstudy => $col_title) {
+        if($study == 62) {
+            $table .="<style>.dal_task>div>span {
+                        display: block;
+                        margin-left: 38px;
+                        color: #5592c6;
+                    }</style>";
+            if ($indexstudy == 6) {
+                $table .= '<td><i class="fas fa-plus-circle fa-fw" id="etnicityPlus" aria-hidden="true" onclick="etnicity_change_icon(this.id)" symbol="0"></i></td>';
+            } else if ($indexstudy != 1) {
+                $table .= '<td class="hide" etnicity="1"></td>';
+            } else if ($indexstudy == 1) {
+                $table .= '<td></td>';
             }
+        }else{
+            $table .="<style>.dal_task>div>span {
+                        display: block;
+                        margin-left: 20px;
+                        color: #5592c6;
+                    }</style>";
+            $table .= '<td>&nbsp;</td>';
         }
-        $table .= '<td></td>';
-        $table .= '</tr>';
+    }
+    $table .= '<td></td>';
+    $table .= '</tr>';
 
     $table .= '</thead>';
 
@@ -257,6 +262,11 @@ if(!empty($_GET['dash']) && ProjectData::startTest($_GET['dash'], '', '', $_SESS
     if ($question == 1) {
         $array_colors = array();
         $max = 0;
+        #COLOR
+        $extras = 2;
+        if($study == 61) {
+            $extras = 3;
+        }
         foreach ($row_questions_1 as $indexQuestion => $question_1) {
             $array_colors[$indexQuestion] = array();
             $tooltipTextArray[$indexQuestion] = array();
@@ -309,13 +319,8 @@ if(!empty($_GET['dash']) && ProjectData::startTest($_GET['dash'], '', '', $_SESS
                 $tooltipTextArray = $multipleCol[0];
                 $array_colors = $multipleCol[1];
             }
-        }
-        #COLOR
-        $extras = 2;
-        if($study == 61) {
-            $extras = 3;
-        }
-        foreach ($row_questions_1 as $indexQuestion => $question_1) {
+
+            #PRINT RESULTS
             $question_popover_content = \Vanderbilt\DashboardAnalysisPlatformExternalModule\returnTopScoresLabels($question_1,$module->getChoiceLabels($question_1, $project_id));
             $question_popover_info = ' <a tabindex="0" role="button" data-container="body" data-toggle="popover" data-trigger="hover" data-placement="top" title="Field: ['.$question_1.']" data-content="'.$question_popover_content.'"><i class="fas fa-info-circle fa-fw infoIcon" aria-hidden="true"></i></a>';
             $table .= '<tr><td class="question">'.$module->getFieldLabel($question_1).$question_popover_info.' <i class="fas fa-chart-bar infoChart" id="DashChart_'.$question_1.'" indexQuestion="'.$indexQuestion.'"></i></td>';
@@ -336,6 +341,36 @@ if(!empty($_GET['dash']) && ProjectData::startTest($_GET['dash'], '', '', $_SESS
                     $table .= '<td style="background-color:' . $color . '" class="' . $class . '" ' . $attibute . '><div class="red-tooltip extraInfoLabel" data-toggle="tooltip" data-html="true" title="' . $tooltipTextArray[$indexQuestion][$i] . '">' . $array_colors[$indexQuestion][$i] . '</div></td>';
                 }
             }
+            $table .= '</tr>';
+        }
+    }else if($question == 2){
+
+        $graph = \Vanderbilt\DashboardAnalysisPlatformExternalModule\getNormalStudyColRate($project_id, $conditionDate, $row_questions_1, $graph, $study, $study_options);
+        $graph = \Vanderbilt\DashboardAnalysisPlatformExternalModule\getMissingStudyColRate($project_id, $conditionDate, $row_questions_1, $graph, $study, $study_options);
+
+        foreach ($row_questions_2 as $indexQuestion => $question_2) {
+            $question_popover_content = "";
+            $question_popover_info = ' <a tabindex="0" role="button" data-container="body" data-toggle="popover" data-trigger="hover" data-placement="top" title="'.$question_2.'" data-content="'.$question_popover_content.'"><i class="fas fa-info-circle fa-fw infoIcon" aria-hidden="true"></i></a>';
+            $table .= '<tr><td class="question">'.ucfirst($question_2)." response ".$question_popover_info.' <i class="fas fa-chart-bar infoChart" id="DashChart_'.$question_1.'" indexQuestion="'.$indexQuestion.'"></i></td>';
+            #TOTAL
+            $table .= "<td>0</td>";
+            foreach ($study_options as $index => $col_title) {
+                $questions = $graph[$question_2][$index];
+                if($questions == ""){
+                    $questions = 0;
+                }
+                $percent = number_format((float)($graph[$question_2][$index] / $graph["total_records"][$index]), 2, '.', '')*100;
+                $tooltipTextArray = $questions." questions answered out of ".$graph["total_records"][$index]." records";
+                $table .= '<td><div class="red-tooltip extraInfoLabel" data-toggle="tooltip" data-html="true" title="' . $tooltipTextArray . '">' .$percent.'</td>';
+            }
+            $questions_missing = $graph[$question_2]["missing"];
+            if($questions_missing == ""){
+                $questions_missing = 0;
+            }
+            $percent = number_format((float)($graph[$question_2]["missing"] / $graph["total_records"]["missing"]), 2, '.', '')*100;
+            $tooltipTextArray = $questions_missing." questions answered out of ".$graph["total_records"]["missing"]." records";
+            $table .= '<td><div class="red-tooltip extraInfoLabel" data-toggle="tooltip" data-html="true" title="' . $tooltipTextArray . '">' .$percent.'</td>';
+
             $table .= '</tr>';
         }
     }else {
