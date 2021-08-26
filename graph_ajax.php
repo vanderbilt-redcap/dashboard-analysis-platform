4fa1a86f32825aedb4ed2a4fb109aafa2b460d93
knil-maloon <linkedfrends@gmail.com>
2021-08-26 15:11:14 -0500

    vulnerabilities fixed

--- graph_ajax.php
+++ graph_ajax.php
@@ -7,13 +7,13 @@ use ExternalModules\ExternalModules;
 include_once ("functions.php");
 require_once (dirname(__FILE__)."/classes/GraphData.php");
 
-$question_1 = $_GET['question_1'];
-$study = $_GET['study'];
-$studyOption = $_GET['studyOption'];
-$study_options = json_decode($_GET['study_options']);
-$conditionDate = $_GET['conditionDate'];
-$question = $_GET['question'];
-$project_id = (int)$_GET['pid'];
+$question_1 = $_REQUEST['question_1'];
+$study = $_REQUEST['study'];
+$studyOption = $_REQUEST['studyOption'];
+$study_options = json_decode($_REQUEST['study_options']);
+$conditionDate = $_REQUEST['conditionDate'];
+$question = $_REQUEST['question'];
+$project_id = $_GET['pid'];
 
 $outcome_labels = $module->getChoiceLabels($question_1, $project_id);
 $topScoreMax = count($outcome_labels);
@@ -44,8 +44,8 @@ $chartgraph = GraphData::graphArrays($graph,$study_options);
 echo json_encode(array(
         'status' => 'success',
         'graph' => json_encode($graph),
-        'studyOption' => htmlspecialchars($studyOption,ENT_QUOTES),
-        'question' => htmlspecialchars($question,ENT_QUOTES),
+        'studyOption' => htmlspecialchars($studyOption, ENT_QUOTES),
+        'question' => htmlspecialchars($question, ENT_QUOTES),
         'chartgraph' => json_encode($chartgraph)
     )
 );
