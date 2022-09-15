ed798041f8a09db824e74ed3d62bc3f762978325
knil-maloon <linkedfrends@gmail.com>
2022-09-15 10:32:18 -0500

    login changes to see if we fix the issue or the links.

--- login.php
+++ login.php
@@ -4,31 +4,6 @@ namespace Vanderbilt\DashboardAnalysisPlatformExternalModule;
 $option = htmlentities($_GET['option'],ENT_QUOTES);
 $record = htmlentities($_GET['record'],ENT_QUOTES);
 ?>
-<div style="padding-top: 40px">
-    <div style="padding-left: 15px;font-size: 20px;color: #404040;"><p>Access EPV At-A-Glance Dashboard</p></div>
-    <div style="padding-left: 15px;color: #404040;"></div>
-
-    <div class="col-md-10">
-        <div class="alert alert-info" id="blue-alert" style="display:none">
-            <strong>Check your email for your custom Access Link. </strong> <br />If you haven't received an email within 5 minutes, your email address may not be registered in the system or you may be registered under a different email.
-        </div>
-        <div class="alert alert-danger" id="hub_error_message" style="display: none;"></div>
-        <p></p>
-        <div class="panel panel-default">
-            <div class="panel-heading"><h3 class="panel-title"><strong>Request Access Link</strong></h3></div>
-            <div class="panel-body">
-                <form role="form" id="form_login">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email Address</label>
-                        <input type="email" style="width:100%;height:34px;border: 1px solid #ccc;padding: 6px 12px;" id="hub_email" name="hub_email" placeholder="Type your email address.">
-                    </div>
-                    <button type="submit" class="btn btn-sm btn-primary" style="font-weight: bold">Get Link</button>
-
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 
 <script>
     $(document).ready(function() {
@@ -58,3 +33,29 @@ $record = htmlentities($_GET['record'],ENT_QUOTES);
         });
     } );
 </script>
+
+<div style="padding-top: 40px">
+    <div style="padding-left: 15px;font-size: 20px;color: #404040;"><p>Access EPV At-A-Glance Dashboard</p></div>
+    <div style="padding-left: 15px;color: #404040;"></div>
+
+    <div class="col-md-10">
+        <div class="alert alert-info" id="blue-alert" style="display:none">
+            <strong>Check your email for your custom Access Link. </strong> <br />If you haven't received an email within 5 minutes, your email address may not be registered in the system or you may be registered under a different email.
+        </div>
+        <div class="alert alert-danger" id="hub_error_message" style="display: none;"></div>
+        <p></p>
+        <div class="panel panel-default">
+            <div class="panel-heading"><h3 class="panel-title"><strong>Request Access Link</strong></h3></div>
+            <div class="panel-body">
+                <form role="form" id="form_login">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email Address</label>
+                        <input type="email" style="width:100%;height:34px;border: 1px solid #ccc;padding: 6px 12px;" id="hub_email" name="hub_email" placeholder="Type your email address.">
+                    </div>
+                    <button type="submit" class="btn btn-sm btn-primary" style="font-weight: bold">Get Link</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
