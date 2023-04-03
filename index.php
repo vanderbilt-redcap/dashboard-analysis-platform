8fa2ea6009204df1e38a51424e916bc86348d938
knil-maloon <linkedfrends@gmail.com>
2023-04-03 11:06:33 -0500

    bug fix

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ use ExternalModules\ExternalModules;
         if( !array_key_exists('token', $_REQUEST) && !array_key_exists('request', $_REQUEST) && empty($_SESSION['token']["EPV".$project_id])){
             include('login.php');
         }else if(!array_key_exists('option', $_REQUEST) && !empty($_SESSION['token']["EPV".$project_id]) && \Vanderbilt\DashboardAnalysisPlatformExternalModule\isTokenCorrect($_SESSION['token']["EPV".$project_id],$project_id_registration)){
-            include_once ('dashboard_private.php?report='.$_GET['report']);
+            include_once ('dashboard_private.php');
         }else if(array_key_exists('option', $_REQUEST) && $option === 'sac' && !empty($_SESSION['token']["EPV".$project_id]) && \Vanderbilt\DashboardAnalysisPlatformExternalModule\isTokenCorrect($_SESSION['token']["EPV".$project_id],$project_id_registration)) {
             include_once('stats_and_charts.php');
         }else{
@@ -107,7 +107,7 @@ use ExternalModules\ExternalModules;
             include('login.php');
         }
     }else{
-        header('Location: '.$module->getUrl('dashboard_public.php?report='.$_GET['report']));
+        header('Location: '.$module->getUrl('dashboard_public.php'));
     }
     ?>
 </div>
