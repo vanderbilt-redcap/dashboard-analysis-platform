b8aa01fa73bab51822d77bee2b8e3a84d316eebf
knil-maloon <linkedfrends@gmail.com>
2023-04-06 13:13:40 -0500

    push cron changes

--- DashboardAnalysisPlatformExternalModule.php
+++ DashboardAnalysisPlatformExternalModule.php
@@ -8,7 +8,32 @@ use ExternalModules\ExternalModules;
 
 class DashboardAnalysisPlatformExternalModule extends AbstractExternalModule
 {
+    /**
+     * This cron will only perform actions once a day between the hours
+     * of midnight and 6am, regardless of how often it is scheduled.
+     * Scheduling it more often is required to ensure it runs at a predictable time.
+     * For example, scheduling it for once every 10 minutes will guarantee that
+     * it runs at the first available minute after 12:10am,
+     * though it may run as soon as 12am.
+     * This produces behavior similar to timed crons.
+     * See the API Sync module's per project sync settings for a more advanced
+     * example of this same concept.
+     */
     function dashboard_cache_file($cronAttributes){
+        $hourRange = 6;
+        if(date('G') > $hourRange){
+            // Only perform actions between 12am and 6am.
+            return;
+        }
+        $lastRunSettingName = 'last-cron-run-time';
+        $lastRun = empty($this->getSystemSetting($lastRunSettingName)) ? $this->getSystemSetting($lastRunSettingName) : 0;
+        $hoursSinceLastRun = (time()-$lastRun)/60/60;
+        if($hoursSinceLastRun < $hourRange){
+            // We're already run recently
+            return;
+        }
+
+        // Perform cron actions here
         foreach ($this->getProjectsWithModuleEnabled() as $project_id){
             try {
                 error_log("dashboardCacheFile PID".$project_id);
@@ -19,7 +44,9 @@ class DashboardAnalysisPlatformExternalModule extends AbstractExternalModule
                 \REDCap::email('datacore@vumc.org', 'datacore@vumc.org',"Cron Error", $e->getMessage());
             }
         }
+        $this->setSystemSetting($lastRunSettingName, time());
     }
+
 
     public function redcap_module_link_check_display($project_id, $link) {
         $privacy = $this->getProjectSetting('privacy');
